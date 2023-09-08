9bf67b23bf4ce7c4c6bf8f4e922a75b4c0ba6743
Tatiana Kalinichenko <tatiankalinichenko@gmail.com>
2023-09-08 19:01:33 +0300

    finished Task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.WriteLine("Введите порядковый номер дня недели");
+int day = Convert.ToInt32 (Console.ReadLine ());
+if (day == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (day == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (day == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (day == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (day == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (day == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (day == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Должно быть, Вы имеете в виду марсианскую неделю, ха-ха.");
+}
