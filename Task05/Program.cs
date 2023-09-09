d1ef40563126cfb106000923d6996ef56f53136b
Tatiana Kalinichenko <tatiankalinichenko@gmail.com>
2023-09-09 13:26:53 +0300

    added for cycle

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 5. Напишите программу, которая 
+// 1. на вход принимает одно число (N), 
+// 2. а на выходе показывает все целые числа в промежутке от -N до N.
+
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите натуральное число");
+int N = Convert.ToInt32(Console.ReadLine());
+for(int i = -N; i <= N; i++)
+{
+    Console.Write($"{i} ");
+}
